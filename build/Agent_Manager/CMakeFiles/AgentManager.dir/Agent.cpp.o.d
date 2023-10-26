d741e77dae6e83a41c4523c5cc5bb81e2ae9ce91
Mohammad Reza Chegini <121607096+Mrchcoin@users.noreply.github.com>
2023-10-27 02:38:22 +0330

    Delete build directory

--- build/Agent_Manager/CMakeFiles/AgentManager.dir/Agent.cpp.o.d
+++
@@ -1,4 +0,0 @@
-Agent_Manager/CMakeFiles/AgentManager.dir/Agent.cpp.o: \
- /home/yoyo/Projects/untitled1/Agent_Manager/Agent.cpp \
- /usr/include/stdc-predef.h \
- /home/yoyo/Projects/untitled1/Agent_Manager/Agent.h
--- build/Agent_Manager/CMakeFiles/AgentManager.dir/AgentManager_autogen/mocs_compilation.cpp.o.d
+++
@@ -1,3 +0,0 @@
-Agent_Manager/CMakeFiles/AgentManager.dir/AgentManager_autogen/mocs_compilation.cpp.o: \
- /home/yoyo/Projects/untitled1/build/Agent_Manager/AgentManager_autogen/mocs_compilation.cpp \
- /usr/include/stdc-predef.h
